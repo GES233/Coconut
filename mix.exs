2095d8193393d6c15dc75a55694144f720990b32
RoastedChestnut <chestnut.roasted@outlook.com>
2026-08-10 22:56:53 +0800

    chore: update version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Coconut.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.2.0"
   @description "An engine-agnostic editor core that treats user intervention as first-class"
 
   def project do
